ae282d2fcc27c6642fea8f652a969c4e9b46601e
Kevin <kevin.sweeney@novartis.com>
2023-03-16 16:52:00 +0000

    FIX: Updated `run.py` for python 3 Print commands without parenthesis

--- run.py
+++ run.py
@@ -13,18 +13,18 @@ os.system('mkdir data/models')
 
 cmd = '''
 		cd preprocessing
-                python gen_master_feature.py --master-file {:s}
+        python gen_master_feature.py --master-file {:s}
 '''.format(args.master_file)
 # python gen_master_feature.py --master-file ../file/master.csv
-print cmd
+print (cmd)
 os.system(cmd)
 
 cmd = '''
 		cd preprocessing
-                python gen_feature_time.py --vital-file {:s}
+        python gen_feature_time.py --vital-file {:s}
 '''.format(args.vital_file)
 # python gen_feature_time.py --vital-file ../file/vital.csv
-print cmd
+print (cmd)
 os.system(cmd)
 
 cmd = '''
@@ -32,7 +32,7 @@ cmd = '''
 		python gen_feature_order.py  --vital-file {:s}
 '''.format(args.vital_file)
 # python gen_vital_feature.py --vital-file ../file/vital.csv
-print cmd
+print (cmd)
 os.system(cmd)
 
 
@@ -42,7 +42,7 @@ cmd = '''
 		python gen_vital_feature.py  --vital-file {:s}
 '''.format(args.vital_file)
 # python gen_vital_feature.py --vital-file ../file/vital.csv
-print cmd
+print (cmd)
 os.system(cmd)
 
 
@@ -51,14 +51,14 @@ cmd = '''
 		python gen_label.py --label-file {:s}
 '''.format(args.label_file)
 # python gen_label.py --label-file ../file/label.csv
-print cmd
+print (cmd)
 os.system(cmd)
 
 cmd = '''
 		cd code
 		python main.py --task {:s}
 '''.format(args.task)
-print cmd
+print (cmd)
 os.system(cmd)
 		
 
